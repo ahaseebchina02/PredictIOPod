7909bb0232c1a6203bd2d96e87560004e01028a2
haseebOptini <ahaseeb91@gmail.com>
2016-08-02 15:16:07 +0500

    Initial commit

---
+++ Example/Pods/Target Support Files/PredictIOPod/PredictIOPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PredictIOPod : NSObject
+@end
+@implementation PodsDummy_PredictIOPod
+@end
---
+++ Example/Pods/Target Support Files/PredictIOPod/PredictIOPod-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double PredictIOPodVersionNumber;
+FOUNDATION_EXPORT const unsigned char PredictIOPodVersionString[];
+
